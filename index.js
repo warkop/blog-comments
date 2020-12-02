b02ce6fc6a221ae9cca202777409157f137dc34a
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-12-02 22:31:01 +0700

    add async

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content });
-  axios.post('http://localhost:4005/events', {
+
+  await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
