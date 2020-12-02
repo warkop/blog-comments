920f048510311e8305ed978ab842d1b4d3bad666
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-12-02 22:14:17 +0700

    part 29

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,6 +21,14 @@ app.post('/posts/:id/comments', (req, res) => {
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content });
+  axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  })
 
   commentsByPostId[req.params.id] = comments;
 
