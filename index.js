40dda71380200116d98766f8900c40a515418792
Mohammad Khafid Barokum <khafid@majoo.id>
2021-02-07 15:56:32 +0700

    updated course 43

--- index.js
+++ index.js
@@ -20,14 +20,17 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
+
+  commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   })
 
@@ -36,8 +39,31 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event: ', req.body.type);
+app.post('/events', async(req, res) => {
+  console.log('Event Received: ', req.body.type);
+
+  const {type,data} = req.body;
+
+  if (type === 'CommentModerated') {
+    const {postId, id, status, content} = data
+    const comments = commentsByPostId[postId];
+
+    const commment = comments.find(comment => {
+      return comment.id === id;
+    });
+
+    commment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
 
   res.send({});
 });
